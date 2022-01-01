10978033fdfd0efabdef00bb11db38647e1e397a
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-01-01 12:47:06 +0530

    linked in share

--- src/components/share-card/index.js
+++ src/components/share-card/index.js
@@ -18,6 +18,16 @@ const ShareCard = ({ title, author, url }) => {
     }, 1500);
   };
 
+  const handleLinkedinShare = () => {
+    window.open(
+      `https://linkedin.com/shareArticle?url=${encodeURIComponent(
+        url
+      )}&mini=true&title=${encodeURIComponent(title)}`,
+      '_blank',
+      'width=600,height=500'
+    );
+  };
+
   return (
     <div className="md:text-center">
       <h3 className="text-gray-700 font-bold text-3xl">Share this article</h3>
@@ -34,16 +44,12 @@ const ShareCard = ({ title, author, url }) => {
         >
           <TwitterLogo weight="bold" />
         </a>
-        <a
+        <button
           className="inline-block p-2 text-red-400"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://www.linkedin.com/shareArticle/?url=${encodeURIComponent(
-            url
-          )}`}
+          onClick={handleLinkedinShare}
         >
           <LinkedinLogo weight="bold" />
-        </a>
+        </button>
         <button className="inline-block p-2 text-red-400" onClick={handleCopy}>
           {icon}
         </button>
